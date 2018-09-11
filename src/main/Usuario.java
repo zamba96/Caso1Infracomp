4ff47dcf189e354353db695456858b6971e6c0e5
CAMILO MARTINEZ <c.martinezc1@uniandes.edu.co>
2018-09-11 17:03:54 -0500

    bosquejo

--- src/main/Usuario.java
+++ src/main/Usuario.java
@@ -2,4 +2,35 @@ package main;
 
 public class Usuario {
 
+	//
+	/* crea el mensaje 
+	 * wait a mensaje 
+	 * yield si no se acepta
+	 *  
+	 */
+	/**
+	 * 
+	 */
+	public Mensaje m;
+	
+	/**
+	 * 
+	 */
+	public Usuario(){
+		m = new Mensaje();
+	}
+	
+	/**
+	 * 
+	 */
+	private void enviar() {
+		//buffer.enviar();
+	}
+	
+	/**
+	 * 
+	 */
+	private void recibir() {
+		//buffer
+	}
 }

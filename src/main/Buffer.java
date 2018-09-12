838651b60689c88a2310e85c0f94e099598e332e
David Patiño <d.patino12@uniandes.edu.co>
2018-09-12 11:23:31 -0500

    Creacion de clase buffer

--- src/main/Buffer.java
+++ src/main/Buffer.java
@@ -1,5 +1,42 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Buffer {
+	private int tamañoBuffer;
+	private ArrayList<Mensaje> buffer;
+	private int clientes;
+	private Object semaforo;
+	
+	public Buffer(int n){
+		tamañoBuffer=n;
+		buffer= new ArrayList<>();
+		clientes=0;
+		semaforo= new Object();
+	}
+	
+	public void enviar(Mensaje n){
+		synchronized (this) {
+			buffer.add(n);
+			clientes++;
+			semaforo.notify();
+		}
+	}
+	public Mensaje atender(){
+		synchronized(this){
+			if(buffer.isEmpty()){
+				try {
+					semaforo.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			return buffer.remove(0);
+			
+		}
+	}
+	
 
 }

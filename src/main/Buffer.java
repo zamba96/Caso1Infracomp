52901bdfe4e6c5d6e40aac480b2d628dec12137f
David Patiño <d.patino12@uniandes.edu.co>
2018-09-12 11:38:08 -0500

    arreglo de variables, documentacion de los metodos, creacion del metodo dartamanoBuffer

--- src/main/Buffer.java
+++ src/main/Buffer.java
@@ -3,25 +3,47 @@ package main;
 import java.util.ArrayList;
 
 public class Buffer {
-	private int tamañoBuffer;
+	/**
+	 * Variables privadas del buffer
+	 */
+	private int tamanoBuffer;
 	private ArrayList<Mensaje> buffer;
 	private int clientes;
 	private Object semaforo;
-	
+	/**
+	 * Metodo constructor de la clase Buffer
+	 * @param n tamaño maximo del buffer
+	 */
 	public Buffer(int n){
-		tamañoBuffer=n;
+		tamanoBuffer=n;
 		buffer= new ArrayList<>();
 		clientes=0;
 		semaforo= new Object();
 	}
+	/**
+	 * Metodo el cual pone los mensajes del cliente en el buffer
+	 * @param n mensaje a encolar en el buffer
+	 * @return true si se encolo el mensaje, false de lo contrario
+	 */
 	
-	public void enviar(Mensaje n){
-		synchronized (this) {
-			buffer.add(n);
-			clientes++;
-			semaforo.notify();
+
+	public boolean enviar(Mensaje n){
+		if(buffer.size()==tamanoBuffer){
+			return false;
+		}else{
+			synchronized (this) {
+				buffer.add(n);
+				clientes++;
+				semaforo.notify();
+				return true;
+			}
 		}
 	}
+	/**
+	 * Metodo el cual le da el mensaje al  servidor para que lo pueda atender
+	 * @return mensaje el cual debe atender el cliente
+	 */
+	
 	public Mensaje atender(){
 		synchronized(this){
 			if(buffer.isEmpty()){
@@ -35,6 +57,19 @@ public class Buffer {
 			}
 			return buffer.remove(0);
 			
+		}
+	}
+	/**
+	 * metodo que me dice si se puede meter mensajes al buffer
+	 * @return true si se puede meter, false de lo contrario
+	 */
+	
+	public synchronized boolean dartamanoBuffer(){
+		if(buffer.size()==tamanoBuffer){
+			return false;
+		}
+		else{
+			return true;
 		}
 	}
 	
